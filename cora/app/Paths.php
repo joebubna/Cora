c4a0498c62ffb0095a5e509145142500938a80b3
Josiah Bubna <josiahbubna@FM-JOSBUB-MAC.local>
2017-02-28 11:54:09 -0800

    Changes to demo paths.

--- cora/app/Paths.php
+++ cora/app/Paths.php
@@ -13,9 +13,5 @@ $paths->add($path);
 
 $path = new \Cora\Path();
 $path->url = 'users/test/{action}-{action2}';
-//$path->def['{action2}'] = '[0-9]+';
 $path->route = '/home/view/{action}/{action2}';
-$path->preExec = function() {
-    return true;
-};
 $paths->add($path);
