23d630bdb10e2a8b71643043c630277b1f6d6d85
Josiah Bubna <josiahbubna@FM-JOSBUB-MAC.local>
2017-03-22 17:09:35 -0700

    Updated demo custom routes.

--- cora/app/Paths.php
+++ cora/app/Paths.php
@@ -1,33 +1,33 @@
 <?php
 $paths = new \Cora\Container();
 
-/** 
+/**
  *  Shows use of a passive route...
  *  This route will get executed, but not end the search for other custom routes.
  *  It WILL however add this route to a list so it can't accidentally be executed a 2nd time.
  */
 $path = new \Cora\Path();
-    $path->url = 'coolbeans/{anything}';
+    $path->url = '/coolbeans/{anything}';
     $path->route = 'tests/{anything}';
     $path->passive = true;
 $paths->add($path);
 
 
-/** 
+/**
  *  Shows use of the special {anything} variable.
  */
 $path = new \Cora\Path();
-    $path->url = 'tests/{anything}';
+    $path->url = '/tests/{anything}';
     $path->route = '/home/view/{anything}';
 $paths->add($path);
 
 
-/** 
- *  Example of flexibility of custom routes. 
+/**
+ *  Example of flexibility of custom routes.
  *  ID variable is specified to be a number via regex.
  */
 $path = new \Cora\Path();
-    $path->url = 'users/{action}-{subaction}/{id}';
+    $path->url = '/users/{action}-{subaction}/{id}';
     $path->def['{id}'] = '[0-9]+';
     $path->route = '/users/forgotPassword/';
 $paths->add($path);
@@ -38,7 +38,7 @@ $paths->add($path);
  *  Also limiting to specific HTTP actions and thirdly disabling RESTful routing for this path.
  */
 $path = new \Cora\Path();
-    $path->url = 'users/test/{action}-{action2}';
+    $path->url = '/users/test/{action}-{action2}';
     $path->route = '/home/view/{action2}/{action}';
     $path->actions = 'GET|POST';
     $path->RESTful = false;
@@ -49,7 +49,7 @@ $paths->add($path);
  *  Example of protecting a route with authentication.
  */
 $path = new \Cora\Path();
-    $path->url = 'home/private';
+    $path->url = '/home/private';
     $path->route = '/home/view/protected/area';
     $path->preExec = function($app) {
         if (!$app->auth->access(new \Models\Auth\LoggedIn)) { return false; }
